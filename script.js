406b457151971c30869f9569ed634707a709e97a
mohamedSabry0 <mohamedsabry3023@gmail.com>
2022-06-28 00:16:32 +0200

    Display data from localStorage

--- script.js
+++ script.js
@@ -1,57 +1,60 @@
-const names = document.getElementById("title");
-const pass = document.getElementById("author");
-const button = document.getElementById("button");
-const list = document.getElementById("list");
+const names = document.getElementById('title');
+const pass = document.getElementById('author');
+const button = document.getElementById('button');
+const list = document.getElementById('list');
+
+list.style.width = '20%';
 
 function books(title, author) {
   this.title = title;
   this.author = author;
 }
 
-let booksArray = [];
+let booksArray = setData() ?? [];
 
 function saveData() {
   const toString = JSON.stringify(booksArray);
   localStorage.setItem('books-list', toString);
 }
 
-function setData(){
+function setData() {
   const toObject = JSON.parse(localStorage.getItem('books-list'));
-  return toObject
-
+  return toObject;
 }
 
 function remove(id) {
-  //  we have index in array, and we can pass it to card
-  // when we click rm btn, to pass back its parent index which is the card
-
- booksArray = booksArray.filter((book, index) => index != id);
+  booksArray = booksArray.filter((_book, index) => index != id);
 }
 
 function display() {
-  list.innerHTML = "";
+  list.innerHTML = '';
   booksArray.forEach((element, index) => {
-    const bookCard = document.createElement("ul");
-    const title = document.createElement("li");
-    const author = document.createElement("li");
-    const removeButton = document.createElement("button");
+    const bookCard = document.createElement('ul');
+    const title = document.createElement('li');
+    const author = document.createElement('li');
+    const removeButton = document.createElement('button');
+    const line = document.createElement('hr');
 
-    bookCard.setAttribute("data-id", index);
+    bookCard.style.listStyle = 'none';
+    bookCard.style.padding = 0;
+    bookCard.setAttribute('data-id', index);
+
     title.textContent = element.title;
     author.textContent = element.author;
+    removeButton.textContent = 'Remove';
 
-    removeButton.addEventListener("click", (e) => {
+    removeButton.addEventListener('click', e => {
       remove(index);
       removeButton.parentElement.remove();
       saveData();
     });
 
     list.appendChild(bookCard);
-    bookCard.append(title, author, removeButton);
+    bookCard.append(title, author, removeButton, line);
   });
 }
 
-button.addEventListener("click", (e) => {
+button.addEventListener('click', e => {
   const book = new books(names.value, pass.value);
   e.preventDefault();
   booksArray.push(book);
@@ -59,4 +62,4 @@ button.addEventListener("click", (e) => {
   display();
 });
 
-
+display();
