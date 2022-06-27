ef306f9c9566dfc1cede9c903e4582e6f656549e
mohamedSabry0 <mohamedsabry3023@gmail.com>
2022-06-27 22:29:07 +0200

    Showing elementsin DOM

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-const names = document.getElementById('exampleInputEmail1')
-const pass = document.getElementById('exampleInputPassword1')
-const button = document.getElementById('button')
+const names = document.getElementById('exampleInputEmail1');
+const pass = document.getElementById('exampleInputPassword1');
+const button = document.getElementById('button');
+const list = document.getElementById('list');
 
-// console.log(names, pass, button)
-
-function books(newName, newPass){
-    this.newName = newName;
-    this.newPass = newPass;
+function books(title, author) {
+  this.title = title;
+  this.author = author;
 }
 
 let booksArray = [];
 
-button.addEventListener('click', (e) =>{
-    const book = new books (names.value, pass.value)
-    e.preventDefault()
-    booksArray.push(book)
-    console.log(booksArray)
-})
+function remove(index) {
+  //  we have index in array, and we can pass it to card
+  // when we click rm btn, to pass back its parent index which is the card
+}
 
 function display() {
+  list.innerHTML = '';
+  booksArray.forEach((element, index) => {
+    const bookCard = document.createElement('ul');
+    const title = document.createElement('li');
+    const author = document.createElement('li');
 
+    bookCard.setAttribute('data-id', index);
+    title.textContent = element.title;
+    author.textContent = element.author;
 
-
-booksArray.forEach(element =>{
-const Ul = document.createElement('ul')
-})
+    list.appendChild(bookCard);
+    bookCard.append(title, author);
+  });
 }
+
+button.addEventListener('click', e => {
+  const book = new books(names.value, pass.value);
+  e.preventDefault();
+  booksArray.push(book);
+
+  display();
+});
