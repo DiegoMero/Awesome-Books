01a8ff68350e5b589b46020882b72499a8a593e6
Diego Mero <diegomero_1999@hotmail.com>
2022-06-28 17:18:33 -0500

    Add classes to the script.js file

--- script.js
+++ script.js
@@ -5,9 +5,41 @@ const list = document.getElementById('list');
 
 list.style.width = '20%';
 
-function Books(title, author) {
-  this.title = title;
-  this.author = author;
+class Books {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+class Display {
+  addBook (book) {
+    list.innerHTML = '';
+    booksArray.forEach((element, index) => {
+      const bookCard = document.createElement('ul');
+      const title = document.createElement('li');
+      const author = document.createElement('li');
+      const removeButton = document.createElement('button');
+      const line = document.createElement('hr');
+
+      bookCard.style.listStyle = 'none';
+      bookCard.style.padding = 0;
+      bookCard.setAttribute('data-id', index);
+
+      title.textContent = element.title;
+      author.textContent = element.author;
+      removeButton.textContent = 'Remove';
+
+      removeButton.addEventListener('click', () => {
+        remove(index);
+        removeButton.parentElement.remove();
+        saveData();
+      });
+
+      list.appendChild(bookCard);
+      bookCard.append(title, author, removeButton, line);
+    });
+  }
 }
 
 function setData() {
@@ -58,8 +90,8 @@ button.addEventListener('click', (e) => {
   const book = new Books(names.value, pass.value);
   e.preventDefault();
   booksArray.push(book);
-  saveData();
-  display();
+  Display.addBook(book);
+  // display();
 });
 
 display();
