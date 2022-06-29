f28b093657ec0db2ce40c11064ba849b7b220b3a
mohamedSabry0 <mohamedsabry3023@gmail.com>
2022-06-29 21:58:06 +0200

    fix linters

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const button = document.getElementById("button");
-const list = document.getElementById("list");
+/* eslint-disable max-classes-per-file */
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const button = document.getElementById('button');
 
 class Book {
   constructor(title, author) {
@@ -10,67 +10,13 @@ class Book {
   }
 }
 
-// class BooksList {
-//   constructor(collection) {
-//     this.collection = collection;
-//   }
-
-//  static add(book) {
-//     BooksList.collection.push(book);
-//   }
-
-//   remove(book) {
-//     this.collection = this.collection.filter(item => item !== book);
-//   }
-// }
-
-// let booksArray = new BooksList(setData() ?? []);
-
-class Display {
-  static displayList() {
-    const booksArray = Storage.getBooks();
-    booksArray.forEach((book) => Display.addBook(book));
-  }
-
-  static addBook(book) {
-    const list = document.getElementById("list");
-    const bookCard = document.createElement("ul");
-    const title = document.createElement("li");
-    const author = document.createElement("li");
-    const removeButton = document.createElement("button");
-    removeButton.classList.add("delete");
-
-    title.innerHTML = book.title;
-    author.textContent = `by ${book.author}`;
-    removeButton.textContent = "Remove";
-
-    bookCard.className = "book-container";
-
-    list.appendChild(bookCard);
-    bookCard.append(title, author, removeButton);
-
-    console.log (title)
-  }
-
-  static removeBook(eTarget) {
-    if (eTarget.classList.contains("delete")) {
-      eTarget.parentElement.remove();
-    }
-  }
-
-  static clearInputFields() {
-    title.value = "";
-    author.value = "";
-  }
-}
-
 class Storage {
   static getBooks() {
     let booksArray;
-    if (localStorage.getItem("books") === null) {
+    if (localStorage.getItem('books') === null) {
       booksArray = [];
     } else {
-      booksArray = JSON.parse(localStorage.getItem("books"));
+      booksArray = JSON.parse(localStorage.getItem('books'));
     }
     return booksArray;
   }
@@ -78,29 +24,61 @@ class Storage {
   static addBookStorage(book) {
     const booksArray = Storage.getBooks();
     booksArray.push(book);
-    localStorage.setItem("books", JSON.stringify(booksArray));
+    localStorage.setItem('books', JSON.stringify(booksArray));
   }
 
   static removeBookStorage(bookTitle) {
     let booksArray = Storage.getBooks();
-    booksArray = booksArray.filter((book) => book.title !== bookTitle);
-    localStorage.setItem("books", JSON.stringify(booksArray));
+    booksArray = booksArray.filter(book => book.title !== bookTitle);
+    localStorage.setItem('books', JSON.stringify(booksArray));
   }
 }
 
-button.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (title.value === "" || author.value === "") {
-    alert("Empty fields");
-  } else {
-    const book = new Book(title.value, author.value);
-    Display.addBook(book);
-    Storage.addBookStorage(book);
-    Display.clearInputFields();
+class Display {
+  static displayList() {
+    const booksArray = Storage.getBooks();
+    booksArray.forEach(book => Display.addBook(book));
   }
+
+  static addBook(book) {
+    const list = document.getElementById('list');
+    const bookCard = document.createElement('ul');
+    const title = document.createElement('li');
+    const author = document.createElement('li');
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('delete');
+
+    title.innerHTML = book.title;
+    author.textContent = `by ${book.author}`;
+    removeButton.textContent = 'Remove';
+
+    bookCard.className = 'book-container';
+
+    list.appendChild(bookCard);
+    bookCard.append(title, author, removeButton);
+  }
+
+  static removeBook(eTarget) {
+    if (eTarget.classList.contains('delete')) {
+      eTarget.parentElement.remove();
+    }
+  }
+
+  static clearInputFields() {
+    title.value = '';
+    author.value = '';
+  }
+}
+
+button.addEventListener('click', e => {
+  e.preventDefault();
+  const book = new Book(title.value, author.value);
+  Display.addBook(book);
+  Storage.addBookStorage(book);
+  Display.clearInputFields();
 });
 
-document.getElementById("list").addEventListener("click", (e) => {
+document.getElementById('list').addEventListener('click', e => {
   Display.removeBook(e.target);
   Storage.removeBookStorage(e.target.parentElement.firstChild.textContent);
 });
